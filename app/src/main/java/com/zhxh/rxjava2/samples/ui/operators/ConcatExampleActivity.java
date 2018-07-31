56ef42cdcf6d8ec6b334970b6a4321f3e2f3226b
zhxh <zhxhcoder@gmail.com>
2018-07-31 18:01:42 +0800

    tst

--- app/src/main/java/com/zhxh/rxjava2/samples/ui/operators/ConcatExampleActivity.java
+++ app/src/main/java/com/zhxh/rxjava2/samples/ui/operators/ConcatExampleActivity.java
@@ -15,7 +15,7 @@ import io.reactivex.Observer;
 import io.reactivex.disposables.Disposable;
 
 /**
- * Created by zhxh on 27/08/16.
+ * Created by zhxh on 2018/1/18
  */
 public class ConcatExampleActivity extends AppCompatActivity {
 
@@ -27,8 +27,8 @@ public class ConcatExampleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_example);
-        btn = (Button) findViewById(R.id.btn);
-        textView = (TextView) findViewById(R.id.textView);
+        btn = findViewById(R.id.btn);
+        textView = findViewById(R.id.textView);
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
